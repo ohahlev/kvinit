ad4b555f8eb0d2e289e3a804ec0d4c84ee3434bf
ahlev <ohahlev@gmail.com>
2019-07-01 01:43:59 +0700

    roll back

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from kivy.app import App
 from kivy.uix.label import Label
-from jnius import autoclass
 
-DisplayMetrics = autoclass('android.util.DisplayMetrics')
-metrics = DisplayMetrics()
 
 class MyApp(App):
 
     def build(self):
-        return Label(text=str(metrics.getDeviceDensity()))
+        return Label(text='Hello world')
+
 
 if __name__ == '__main__':
     MyApp().run()
